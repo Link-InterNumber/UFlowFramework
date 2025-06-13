0482bb780d4538dc98fd8bbc4284c3b7fd968cc5
Link <85669660+Link-InterNumber@users.noreply.github.com>
2025-06-13 09:19:17 +0800

    update NetClientManager

---
+++ Systems/NetWorking/NetClientManager_Log.cs
@@ -0,0 +1,42 @@
+using PowerCellStudio;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GameProtocol
+{
+    public partial class NetClientManager
+    {
+        private Queue<QueueLog> _logQueue = new Queue<QueueLog>();
+
+        private void QueueLog(QueueLogLevel logLevel, string message)
+        {
+            var log = new QueueLog()
+            {
+                logLevel = logLevel,
+                logMessage = message
+            };
+            _logQueue.Enqueue(log);
+        }
+
+        private void DisplayLog()
+        {
+            while (_logQueue.Count > 0)
+            {
+                var log = _logQueue.Dequeue();
+                switch (log.logLevel)
+                {
+                    case QueueLogLevel.Info:
+                        NetWorkLog.Log(log.logMessage);
+                        break;
+                    case QueueLogLevel.Warning:
+                        NetWorkLog.LogWarning(log.logMessage);
+                        break;
+                    case QueueLogLevel.Error:
+                        NetWorkLog.LogError(log.logMessage);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Systems/NetWorking/NetClientManager_Message.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GameProtocol
+{
+    public partial class NetClientManager
+    {
+
+#region 消息监听注册
+
+        private Dictionary<Type, IMessageReceiveHandler> _listenerHandlers = new Dictionary<Type, IMessageReceiveHandler>();
+
+        public void AddMessageListener<T>(BaseLinkAction<T> onReceived)
+            where T : class, global::ProtoBuf.IExtensible
+        {
+            _listenerHandlers.TryGetValue(typeof(T), out var handler);
+            if(handler == null)
+            {
+                handler = new MessageReceiveHandler<T>();
+                _listenerHandlers.Add(typeof(T), handler);
+            }
+            var messageReceiveHandler = handler as MessageReceiveHandler<T>;
+            messageReceiveHandler?.AddListener(onReceived);
+        }
+        
+        public void RemoveMessageListener<T>(BaseLinkAction<T> onReceived)
+            where T : class, global::ProtoBuf.IExtensible
+        {
+            _listenerHandlers.TryGetValue(typeof(T), out var handler);
+            if(handler == null)
+            {
+                return;
+            }
+            var messageReceiveHandler = handler as MessageReceiveHandler<T>;
+            if (messageReceiveHandler == null) return;
+            messageReceiveHandler.RemoveListener(onReceived);
+            if(messageReceiveHandler.EventListenerCount == 0)
+            {
+                _listenerHandlers.Remove(typeof(T));
+            }
+        }
+
+#endregion
+
+#region 消息发送
+
+        private void SendAsync(byte[] message)
+        {
+            for (int i = 0; i < 1 + _repeatMessage; i++)
+            {
+                _client.SendAsync(message);
+            }
+        }
+
+        private class SendDataBuffer : IDisposable
+        {
+            private byte[] _buffer;
+            public byte[] Buffer => _buffer;
+            private IMessageReceiveHandler _handler;
+            public IMessageReceiveHandler Handler => _handler;
+            private Type _messageType;
+            public Type MessageType => _messageType;
+
+            public SendDataBuffer(Type messageType, byte[] buffer, IMessageReceiveHandler handler)
+            {
+                _buffer = buffer;
+                _handler = handler;
+                _messageType = messageType;
+            }
+
+            public void Dispose()
+            {
+                _buffer = null;
+                _handler = null;
+                _messageType = null;
+            }
+        }
+
+        private List<SendDataBuffer> _sendDataBuffers = new List<SendDataBuffer>();
+
+        private Dictionary<Type, IMessageReceiveHandler> _waitHandlers = new Dictionary<Type, IMessageReceiveHandler>();
+
+        public void Send<T>(T message)
+            where T : class, global::ProtoBuf.IExtensible
+        {
+            if (_client == null || !_client.IsConnected || message == null)
+            {
+                return;
+            }
+            var buffer = NetworkSerializer.Serialize(message);
+            SendAsync(buffer);
+        }
+        
+        public MessageReceiveHandler<T> SendQueue<T, TK>(TK message) 
+            where T : class, global::ProtoBuf.IExtensible
+            where TK : class, global::ProtoBuf.IExtensible
+        {
+            if (_client == null || !_client.IsConnected || message == null)
+            {
+                return null;
+            }
+            var buffer = NetworkSerializer.Serialize(message);
+            var handler = new MessageReceiveHandler<T>(true);
+            var messageType = typeof(T);
+            if (_waitHandlers.ContainsKey(messageType))
+            {
+                _sendDataBuffers.Add(new SendDataBuffer(messageType, buffer, handler));
+                return handler;
+            }
+            _waitHandlers.Add(messageType, handler);
+            SendAsync(buffer);
+            return handler;
+        }
+
+        private void SendQueueBuffer()
+        {
+            if (_sendDataBuffers.Count <= 0) return;
+            for (var i = 0; i < _sendDataBuffers.Count;)
+            {
+                var sendDataBuffer = _sendDataBuffers[i];
+                if (_waitHandlers.ContainsKey(sendDataBuffer.MessageType))
+                {
+                    i++;
+                    continue;
+                }
+                _waitHandlers.Add(sendDataBuffer.MessageType, sendDataBuffer.Handler);
+                SendAsync(sendDataBuffer.Buffer);
+                sendDataBuffer.Dispose();
+                _sendDataBuffers.RemoveAt(i);
+            }
+        }
+
+        private void HandleReceivedPackages()
+        {
+            if (!_client.HasEnqueuedPackages()) return;
+            while (_client.HasEnqueuedPackages())
+            {
+                var length = _client.GetNextPackage(ref _buffer);
+                if (length <= 0) continue;
+                var message = NetworkSerializer.Deserialize(_buffer, length, out var messageType);
+                if (message == null) continue;
+                if (_listenerHandlers.TryGetValue(messageType, out var handler))
+                {
+                    handler.OnReceived(message, messageType);
+                }
+
+                if (_waitHandlers.TryGetValue(messageType, out var waitHandler))
+                {
+                    waitHandler.OnReceived(message, messageType);
+                    _waitHandlers.Remove(messageType);
+                }
+            }
+        }
+
+#endregion
+    }
+}
