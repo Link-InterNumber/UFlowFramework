45873619976c22b9aa29d6a2aa8d4eba08b6acb9
Link <85669660+Link-InterNumber@users.noreply.github.com>
2025-11-14 17:10:44 +0800

    update 文档更新

--- Systems/UISystem/Base/Interface/ICacheablePage.cs
+++ Systems/UISystem/Base/Interface/ICacheablePage.cs
@@ -2,6 +2,8 @@ namespace PowerCellStudio
 {
     public interface ICacheablePage
     {
+        // Page暂存的时间（秒）
+        // Page cache duration (seconds)
         public float retainTime { get; }
     }
 }
